f567e5ded0c7b7388f651fb9df7af4548df3132d
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2024-02-08 04:46:29 -0500

    add framer motion experiments with .title and h2

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,25 +6,66 @@ import {
   StyledImage,
   Hide,
 } from "../styles.js";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  // const titleAnim = {
+  //   hidden: { opacity: 0 },
+  //   show: {
+  //     opacity: 1,
+  //     transition: { duration: 2 },
+  //   },
+  // };
+  // const container = {
+  //   hidden: { x: 100 },
+  //   show: {
+  //     x: 0,
+  //     transition: {
+  //       duration: 3,
+  //       ease: "easeOut",
+  //       staggerChildren: 1,
+  //     },
+  //   },
+  // };
   return (
     <StyledAbout>
       <Description>
-        <div className="title">
+        <motion.div
+        // variants={container}
+        // initial="hidden"
+        // animate="show"
+        // className="title"
+        >
           <Hide>
-            <h2>We work to make </h2>
+            <motion.h2
+            // animate={{
+            //   opacity: 1,
+            //   transition: {
+            //     duration: 5,
+            //   },
+            // }}
+            // initial={{ opacity: 0 }}
+            // variants={titleAnim}
+            >
+              We work to make{" "}
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2
+            // variants={titleAnim}
+            >
               your <span>dreams</span>{" "}
               come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true. </h2>
+            <motion.h2
+            // variants={titleAnim}
+            >
+              true.{" "}
+            </motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography
           or videography needs that you
